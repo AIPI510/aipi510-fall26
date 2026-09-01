c283768863255c6de2d33511abb5d1c1ebc984d6
Xianyu Wang <sigridwang@XianyudeMacBook-Air.local>
2026-09-01 14:11:03 -0400

    update for web2

--- week2/web/XYWang-sunset.py
+++ week2/web/XYWang-sunset.py
@@ -1,17 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from io import StringIO
 
 url = "https://www.basketball-reference.com/leagues/NBA_2024_totals.html"
-
 response =requests.get(url)
-
 soup = BeautifulSoup(response.text,"html.parser")
 tables = soup.find_all("table")
 
-df = pd.read_html(str(tables[0]))[0]
-df = df.dropna(how="all")
-df = df.reset_index(drop=True)
+df = pd.read_html(StringIO(str(tables[0])))[0]
 
 print("First 5 rows:")
 print(df.head())
