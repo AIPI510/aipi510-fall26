bd37e5750d3ed83377558261e038ee832bb7d606
Shenwei Zhang <sz381@duke.edu>
2026-09-01 14:01:28 -0400

    feat(week2/api): add get_from_api functions that get data from api and return the dataframe with relevant keys

--- week2/arduino/get_arduino_data.py
+++ week2/arduino/get_arduino_data.py
@@ -1,8 +1,25 @@
 import serial
 import time
-ser = serial.Serial('/dev/tty.usbmodem11401', 9600)  # Replace with your port, Run in terminal:ls /dev/tty.*
+import csv
+
+ser = serial.Serial('/dev/tty.usbmodem3101', 9600)  # Replace with your port, Run in terminal:ls /dev/tty.*
 time.sleep(2)  # Wait for connection
+
+output = []
+
 for i in range(10):  # Read 10 lines
     line = ser.readline().decode('utf-8').strip()
     print(line)
+    row = [value.strip() for value in line.split(",")]
+    output.append(row)
+
 ser.close()
+
+
+def parse_output_csv(op: list):
+    with open("output.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(op)
+
+
+parse_output_csv(output)
