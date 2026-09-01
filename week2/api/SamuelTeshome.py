249c82014cc794e73e3c92c3e3bcf5957414ef4c
Samuel Teshome <samuel.teshome@duke.edu>
2026-09-01 14:06:17 -0400

    API update

--- week2/api/SamuelTeshome.py
+++ week2/api/SamuelTeshome.py
@@ -13,3 +13,6 @@ df = pd.DataFrame(data)
 pd.set_option('display.max_columns', None )
 
 print(df.head(5))
+
+# Pulls Hopsital Certification data on hospitals in every state. Giving an score for percent of hospitals meeting various criteria
+# Could use this tool to understand hopspital compliance and how that may influence patient outcomes.
